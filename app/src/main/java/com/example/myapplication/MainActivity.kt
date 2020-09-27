720a662bfe6e9388e460026ce609b2ef57d7be8b
albertng123 <39754511+albertng123@users.noreply.github.com>
2020-09-27 21:58:35 +0700

    Improve : setText Button (#1)
    * Improve : setText Button
    
    * Improve : set Condition of Button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,21 +1,49 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var valueA = Random.nextInt(0,100)
+    var valueB = Random.nextInt(0,100)
+    var valueC = Random.nextInt(0,100)
+    var valueD = Random.nextInt(0,100)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonE = findViewById<Button>(R.id.button1)
+        var valueE = valueA + valueB
+        var valueF = valueC - valueD
+        buttonE.setText("${valueA} + ${valueB}").toString()
+        buttonE.setOnClickListener {
+            if(valueE > valueF){
+                Toast.makeText(this, "Benar", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Salah", Toast.LENGTH_SHORT).show()
+            }
+        }
 
-        var rand1 = Random.nextInt(0,100)
-        print(rand1)
-        button1.setText(rand1)
+        val buttonF = findViewById<Button>(R.id.button2)
+        buttonF.setText("${valueC} - ${valueD}").toString()
+        buttonF.setOnClickListener {
+            if(valueF > valueE){
+                Toast.makeText(this, "Benar", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Salah", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
-    //    val but1 = view.findViewById<Button>(R.id.button1)
-
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+    }
 }
